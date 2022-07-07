886a16885b7150c322ac0bf48c33c374e687c9cc
RichardPickman <drezzerock@gmail.com>
2022-07-07 17:39:19 +0200

    feat: add settings

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": ['stylelint-config-recommended-scss', 'stylelint-config-prettier'],
+    "plugins": ["stylelint-scss"],
+    "rules": {
+        'no-descending-specificity': 'off',
+    }
+    
+}
