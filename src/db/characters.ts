b1791c1e17884fbbccd21251311887793e62ef4b
RichardPickman <drezzerock@gmail.com>
2022-07-14 17:42:56 +0200

    feat: add dumb db

---
+++ src/db/characters.ts
@@ -0,0 +1,84 @@
+const characters = [
+    {
+        id: 1,
+        name: 'Mike',
+        season: [1,2,3,4],
+    },
+    {
+        id: 2,
+        name: 'Eleven',
+        season: [1,2,3,4],
+    },
+    {
+        id: 3,
+        name: 'Will',
+        season: [1,2,3,4],
+    },
+    {
+        id: 4,
+        name: 'Max',
+        season: [2,3,4],
+    },
+    {
+        id: 5,
+        name: 'Lucas',
+        season: [1,2,3,4],
+    },
+    {
+        id: 6,
+        name: 'Steve',
+        season: [1,2,3,4],
+    },
+    {
+        id: 7,
+        name: 'Nancy',
+        season: [1,2,3,4],
+    },
+    {
+        id: 8,
+        name: 'Dustin',
+        season: [1,2,3,4],
+    },
+    {
+        id: 9,
+        name: 'Jonathan',
+        season: [1,2,3,4],
+    },
+    {
+        id: 10,
+        name: 'Hopper',
+        season: [1,2,3,4],
+    },
+    {
+        id: 11,
+        name: 'Joyce',
+        season: [1,2,3,4],
+    },
+    {
+        id: 12,
+        name: 'Murray',
+        season: [1,2,3,4],
+    },
+    {
+        id: 13,
+        name: 'Billy',
+        season: [2,3,4],
+    },
+    {
+        id: 14,
+        name: 'Eddie',
+        season: [4],
+    },
+    {
+        id: 15,
+        name: 'Robin',
+        season: [3, 4],
+    },
+    {
+        id: 16,
+        name: 'Erica',
+        season: [2, 3, 4],
+    },
+]
+
+export default characters;
---
+++ src/db/images.ts
@@ -0,0 +1,100 @@
+import bomber from '../assets/images/common/bomber.jpg';
+import camaro from '../assets/images/common/camaro.png';
+import dnd from '../assets/images/common/dnd.png';
+import faberge from '../assets/images/common/faberge.png';
+import guitar from '../assets/images/common/guitar.png';
+import lights from '../assets/images/common/lights.png';
+import scoops from '../assets/images/common/scoops-ahoy.png';
+import skate from '../assets/images/common/skate.png';
+import thinking_cap from '../assets/images/common/thinking_cap.png';
+import vodka from '../assets/images/common/vodka.png';
+import metallica from '../assets/images/common/metallica.png';
+import motley from '../assets/images/common/motley.png';
+import eggos from '../assets/images/common/eggos.png';
+
+
+import AKMSU from '../assets/images/guns/AKMSU.png';
+import Bushmaster from '../assets/images/guns/Bushmaster.png';
+import Colt from '../assets/images/guns/Colt.png';
+import Ithaca from '../assets/images/guns/Ithaca.png';
+import Mossberg from '../assets/images/guns/Mossberg.png';
+import MP5KA3 from '../assets/images/guns/MP5KA3.png';
+import Remington from '../assets/images/guns/Remington.png';
+import SmithModel10 from '../assets/images/guns/Smith10.png';
+import SmithModel66 from '../assets/images/guns/Smith66.png';
+import doubleBarrel from '../assets/images/guns/doubleBarrel.png';
+import Winchester from '../assets/images/guns/Winchester.png';
+
+
+import allSeasons from '../assets/images/posters/allSeasons.png';
+import elevenPoster from '../assets/images/posters/eleven-poster.png';
+import halloweenHorror from '../assets/images/posters/halloween-horror-nighs.png';
+import hawkinsSchool from '../assets/images/posters/hawkins-school.png';
+import notIn from '../assets/images/posters/not-in-hawkins.png';
+import season2 from '../assets/images/posters/season2.png';
+import oneSummer from '../assets/images/posters/season3.png';
+import elevenSolo from '../assets/images/posters/stranger-things-summer-teaser-posters-season-3-01-e6387wp0hq.png';
+import maxSolo from '../assets/images/posters/stranger-things-summer-teaser-posters-season-3-02-1y0gj9xf5z.png';
+import steveSolo from '../assets/images/posters/stranger-things-summer-teaser-posters-season-3-03-8v46517741.png';
+import dustinSolo from '../assets/images/posters/stranger-things-summer-teaser-posters-season-3-04-4278q72ac5.png';
+import robinSolo from '../assets/images/posters/stranger-things-summer-teaser-posters-season-3-05-z09777c7f8.png';
+import ericaSolo from '../assets/images/posters/stranger-things-summer-teaser-posters-season-3-06-02t89lp895.png';
+import hopperSolo from '../assets/images/posters/stranger-things-summer-teaser-posters-season-3-07-14nk26068u.png';
+import joyceSolo from '../assets/images/posters/stranger-things-summer-teaser-posters-season-3-08-io92572459.png';
+import nancySolo from '../assets/images/posters/stranger-things-summer-teaser-posters-season-3-09-p338148jr2.png';
+import jonathanSolo from '../assets/images/posters/stranger-things-summer-teaser-posters-season-3-10-9340h1k9t7.png';
+import billySolo from '../assets/images/posters/stranger-things-summer-teaser-posters-season-3-11-s36s6n0f4w.png';
+import lucasSolo from '../assets/images/posters/stranger-things-summer-teaser-posters-season-3-12-5tnpy88e23.png';
+import mikeSolo from '../assets/images/posters/stranger-things-summer-teaser-posters-season-3-13-923b39w49u.png';
+import willSolo from '../assets/images/posters/stranger-things-summer-teaser-posters-season-3-14-q48c687312.png';
+
+
+const imgs = { 
+    bomber, 
+    camaro, 
+    dnd, 
+    faberge, 
+    guitar, 
+    lights, 
+    scoops, 
+    skate, 
+    thinking_cap, 
+    vodka,
+    motley,
+    metallica,
+    eggos,
+    AKMSU, 
+    Bushmaster, 
+    Colt, 
+    Ithaca, 
+    Mossberg, 
+    MP5KA3, 
+    Remington, 
+    SmithModel10, 
+    SmithModel66, 
+    doubleBarrel, 
+    Winchester, 
+    allSeasons, 
+    elevenPoster, 
+    halloweenHorror, 
+    hawkinsSchool, 
+    notIn, 
+    season2, 
+    oneSummer, 
+    elevenSolo, 
+    maxSolo, 
+    steveSolo, 
+    dustinSolo, 
+    robinSolo, 
+    ericaSolo, 
+    hopperSolo,
+    joyceSolo,
+    nancySolo, 
+    jonathanSolo, 
+    billySolo, 
+    lucasSolo, 
+    mikeSolo, 
+    willSolo
+}
+
+export default imgs;
